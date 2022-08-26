2323c510e523bc28e570830f78828323c78c3d3a
vyang14 <vyang1014888@gmail.com>
2022-08-26 04:42:36 -0400

    models created according to README

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,22 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: 'product',
+      key: 'id',
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: 'tag',
+      key: 'id',
+    },
   },
   {
     sequelize,
